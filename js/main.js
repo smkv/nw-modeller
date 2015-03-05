d26d972cebb04831b0312ad52bc0600fbcc09e94
smkv <andrei.samkov@gmail.com>
2015-03-05 16:19:11 +0200

    A simple text document

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 var fs = require('fs');
 
 
+var repositoryLocation = './repository/';
+
+
 $(function() {
 
 
@@ -10,26 +13,81 @@ $(function() {
   });
 
 
+  function getFileItemDetails(path) {
+    var fullPath = repositoryLocation + path;
+    var stat = fs.lstatSync(fullPath);
+
+    var icon = 'fa fa-folder-open';
+    var name = path.substring(path.indexOf('/'));
+    var type = 'folder';
+
+    if (stat.isFile()) {
+      type = 'file';
+      icon = 'fa fa-file-o';
+      var data = fs.readFileSync(fullPath);
+      var firstLine = new String(data).split('\n', 1)[0];
+      var regExp = /\[(\w+)]\s*(.+)\s*/;
+      if (regExp.test(firstLine)) {
+        matches = regExp.exec(firstLine);
+        type = matches[1];
+        name = matches[2];
+
+        switch (type) {
+          case 'document':
+            icon = 'fa fa-file-text-o';
+            break;
+        }
+
+      }
+    }
+
+    return {
+      name: name,
+      path: path,
+      real_path: fs.realpathSync(fullPath),
+      directory: stat.isDirectory(),
+      icon: icon,
+      type: type
+    };
+  }
 
 
   $('#sidebar-tree').jstree({
                               core: {
-                                data: function(node , callback){
-                                  var path = node.id === '#' ? '.' : node.id;
+                                multiple: false,
+                                data: function(node, callback) {
+                                  var path = node.id === '#' ? '' : node.id;
                                   var files = [];
-                                  $.each(fs.readdirSync(path), function(index, file) {
-                                    var isADirectory = fs.statSync(path + '/' + file).isDirectory();
+                                  $.each(fs.readdirSync(repositoryLocation + path), function(index, file) {
+                                    var details = getFileItemDetails(path + '/' + file);
                                     files.push({
-                                                 id: path + '/' + file,
-                                                 text: file,
-                                                 icon: isADirectory ? 'glyphicon glyphicon-folder-open' : 'glyphicon glyphicon-file',
-                                                 children: isADirectory
+                                                 id: details.path,
+                                                 text: details.name,
+                                                 icon: details.icon,
+                                                 children: details.directory,
+                                                 details: details
                                                });
                                   });
                                   callback(files);
                                 }
                               }
-                            });
+                            })
+    .on('changed.jstree', function(e, data) {
+          if (data && data.selected && data.selected.length) {
+            var id = data.selected[0];
+            var details = getFileItemDetails(id);
+            var panel = $('#entry-panel');
+            panel.find('.panel-heading').html('<i class="' + details.icon + '"></i> ' + details.name);
+            panel.find('.panel-footer').html(details.real_path);
+            
+            var data = fs.readFileSync(details.real_path);
+            var split = new String(data).split('\n');
+            var content = split.slice(1).join('<br />');
+
+            panel.find('.panel-body').html(content);
+          }
+
+        });
 
 
 });
