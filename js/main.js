ffe6c994864e458d8e46591f814f31627c9fedd9
smkv <andrei.samkov@gmail.com>
2015-03-05 21:13:20 +0200

    Open/Close icon for a folder

--- js/main.js
+++ js/main.js
@@ -4,105 +4,113 @@ var fs = require('fs');
 var repositoryLocation = './repository/';
 
 
-$(function() {
+$(function () {
 
 
-  $('[data-toggle=offcanvas]').click(function() {
-    $('.row-offcanvas').toggleClass('active');
-    $(this).toggleClass('opened');
-  });
+    $('[data-toggle=offcanvas]').click(function () {
+        $('.row-offcanvas').toggleClass('active');
+        $(this).toggleClass('opened');
+    });
 
 
-  function getFileItemDetails(path) {
-    var fullPath = repositoryLocation + path;
-    var stat = fs.lstatSync(fullPath);
+    function getFileItemDetails(path) {
+        var fullPath = repositoryLocation + path;
+        var stat = fs.lstatSync(fullPath);
 
-    var icon = 'fa fa-folder-open';
-    var name = path.substring(path.lastIndexOf('/') + 1);
-    var type = 'folder';
+        var icon = 'fa fa-folder';
+        var name = path.substring(path.lastIndexOf('/') + 1);
+        var type = 'folder';
 
-    if (stat.isFile()) {
-      type = 'file';
-      icon = 'fa fa-file-o';
-      var data = fs.readFileSync(fullPath);
-      var firstLine = new String(data).split('\n', 1)[0];
-      var regExp = /\[(\w+)]\s*(.+)\s*/;
-      if (regExp.test(firstLine)) {
-        matches = regExp.exec(firstLine);
-        type = matches[1];
-        name = matches[2];
+        if (stat.isFile()) {
+            type = 'file';
+            icon = 'fa fa-file-o';
+            var data = fs.readFileSync(fullPath);
+            var firstLine = new String(data).split('\n', 1)[0];
+            var regExp = /\[(\w+)]\s*(.+)\s*/;
+            if (regExp.test(firstLine)) {
+                matches = regExp.exec(firstLine);
+                type = matches[1];
+                name = matches[2];
 
-        switch (type) {
-          case 'document':
-            icon = 'fa fa-file-text-o';
-            break; 
-          case 'component':
-            icon = 'fa fa-folder';
-            break;
-          
-          case 'requirement':
-            icon = 'fa fa-bolt';
-            break;
-          
-          case 'diagram':
-            icon = 'fa fa-picture-o';
-            break;
+                switch (type) {
+                    case 'document':
+                        icon = 'fa fa-file-text-o';
+                        break;
+                    case 'component':
+                        icon = 'fa fa-folder-o';
+                        break;
+
+                    case 'requirement':
+                        icon = 'fa fa-bolt';
+                        break;
+
+                    case 'diagram':
+                        icon = 'fa fa-picture-o';
+                        break;
+                }
+
+            }
         }
 
-      }
+        return {
+            name: name,
+            path: path,
+            real_path: fullPath,
+            directory: stat.isDirectory(),
+            icon: icon,
+            type: type
+        };
     }
 
-    return {
-      name: name,
-      path: path,
-      real_path: fs.realpathSync(fullPath),
-      directory: stat.isDirectory(),
-      icon: icon,
-      type: type
-    };
-  }
 
-
-  $('#sidebar-tree').jstree({
-                              core: {
-                                multiple: false,
-                                data: function(node, callback) {
-                                  var path = node.id === '#' ? '' : node.id;
-                                  var files = [];
-                                  $.each(fs.readdirSync(repositoryLocation + path), function(index, file) {
-                                    var details = getFileItemDetails(path + '/' + file);
-                                    files.push({
-                                                 id: details.path,
-                                                 text: details.name,
-                                                 icon: details.icon,
-                                                 children: details.directory,
-                                                 details: details
-                                               });
-                                  });
-                                  callback(files);
-                                }
-                              }
-                            })
-    .on('changed.jstree', function(e, data) {
-          if (data && data.selected && data.selected.length) {
-            var id = data.selected[0];
-            var details = getFileItemDetails(id);
-            var panel = $('#entry-panel');
-            panel.find('.panel-heading').html('<i class="' + details.icon + '"></i> ' + details.name);
-            panel.find('.panel-footer').html('<small class="text-muted">' + details.real_path + '</small>');
-
-            if (!details.directory) {
-              var data = fs.readFileSync(details.real_path);
-              var split = new String(data).split('\n');
-              var content = split.slice(1).join('<br />');
-
-              panel.find('.panel-body').html(content);
-            }else{
-              panel.find('.panel-body').html('Package details will be here');
-              
+    $('#sidebar-tree').jstree({
+        core: {
+            multiple: false,
+            data: function (node, callback) {
+                var path = node.id === '#' ? '' : node.id;
+                var files = [];
+                $.each(fs.readdirSync(repositoryLocation + path), function (index, file) {
+                    var details = getFileItemDetails(path + '/' + file);
+                    files.push({
+                        id: details.path,
+                        text: details.name,
+                        icon: details.icon,
+                        children: details.directory,
+                        details: details
+                    });
+                });
+                callback(files);
             }
-          }
+        }
+    })
+        .on('changed.jstree', function (e, data) {
+            if (data && data.node) {
+                var details = data.node.original.details;
+                var panel = $('#entry-panel');
+                panel.find('.panel-heading').html('<i class="' + details.icon + '"></i> ' + details.name);
+                panel.find('.panel-footer').html('<small class="text-muted">' + details.real_path + '</small>');
 
+                if (!details.directory) {
+                    var data = fs.readFileSync(details.real_path);
+                    var split = new String(data).split('\n');
+                    var content = split.slice(1).join('<br />');
+
+                    panel.find('.panel-body').html(content);
+                } else {
+                    panel.find('.panel-body').html('Package details will be here');
+
+                }
+            }
+
+        }).on('open_node.jstree close_node.jstree', function (e, data) {
+            console.log(data);
+            if (data && data.node) {
+                var details = data.node.original.details;
+                if (details.directory) {
+                    var icon = data.node.state.opened ? 'fa fa-folder-open' : 'fa fa-folder';
+                    data.instance.set_icon(data.node, icon);
+                }
+            }
         });
 
 
